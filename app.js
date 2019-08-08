07263d1fc1f269d893b3ac0c5c827580dc84d846
Thanigaivasan <thanigaivasan27@gmail.com>
2019-08-08 14:29:29 +0530

    Added the initial files

---
+++ app.js
@@ -0,0 +1,13 @@
+var express = require('express');
+
+var app = express();
+
+var port = process.env.port || 3000;
+
+app.get('/',(req,res) => {
+    res.send("Hello and Welcome to my API !!!!");
+})
+
+app.listen(port, () => {
+    console.log("Server is running on "+port);
+});
