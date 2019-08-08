6688f84652ecbdbf71f2924babb4abdd9af2b718
Thanigaivasan <thanigaivasan27@gmail.com>
2019-08-08 15:03:51 +0530

    added prettier extension

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-var express = require('express');
+var express = require("express");
 
 var app = express();
 
 var port = process.env.port || 3000;
 
-app.get('/',(req,res) => {
-    res.send("Hello and Welcome to my API !!!!");
-})
+app.get("/", (req, res) => {
+  res.send("Hello and Welcome to my API !!!!");
+});
 
 app.listen(port, () => {
-    console.log("Server is running on "+port);
+  console.log("Server is running on " + port);
 });
